ac9bd798b543e70c5d1c478e939dcb1b78e47eef
Ezrul Fazwan <ezrul@stackideas.com>
2022-09-02 10:51:08 +0800

    Fixed deprecated Text.propTypes.styles for react native 0.69

--- ScrollableTabBar.js
+++ ScrollableTabBar.js
@@ -1,6 +1,6 @@
 const React = require('react');
 const ReactNative = require('react-native');
-const { ViewPropTypes } = require('deprecated-react-native-prop-types');
+const { ViewPropTypes, TextPropTypes } = require('deprecated-react-native-prop-types');
 const PropTypes = require('prop-types');
 const createReactClass = require('create-react-class');
 const {
@@ -28,7 +28,7 @@ const ScrollableTabBar = createReactClass({
     style: ViewPropTypes.style,
     tabStyle: ViewPropTypes.style,
     tabsContainerStyle: ViewPropTypes.style,
-    textStyle: Text.propTypes.style,
+    textStyle: TextPropTypes.style,
     renderTab: PropTypes.func,
     underlineStyle: ViewPropTypes.style,
     onScroll: PropTypes.func,
